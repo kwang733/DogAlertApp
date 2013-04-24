ba24f34306f15eefb53d8ee529db7e1f2f2fcb15
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 21:59:09 -0300

    Update Activities.h

--- DoggyAlert/DoggyAlert/Activities.h
+++ DoggyAlert/DoggyAlert/Activities.h
@@ -1,11 +1,3 @@
-//
-//  Activities.h
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
