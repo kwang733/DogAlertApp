8955332d1990bf0dbe98af18b3366b9daada4e0c
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:04:10 -0300

    Update UITweeksClass.h

--- DoggyAlert/DoggyAlert/UITweeksClass.h
+++ DoggyAlert/DoggyAlert/UITweeksClass.h
@@ -1,11 +1,3 @@
-//
-//  UITweeksClass.h
-//  Behave
-//
-//  Created by Syed Ayaz Uddin Alavi on 9/23/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 @class NavigationController;
 @class CustomViewController;
