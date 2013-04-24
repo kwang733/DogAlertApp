c6c9cfb3d7cd31e8cd4e013f60593e627817e8ae
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:03:19 -0300

    Update Dogs.h

--- DoggyAlert/DoggyAlert/Dogs.h
+++ DoggyAlert/DoggyAlert/Dogs.h
@@ -1,11 +1,3 @@
-//
-//  Dogs.h
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
