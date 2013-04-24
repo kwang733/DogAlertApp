34a09bc2ec4bc5bc4afb019f17c7a2579f8bccd1
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:04:35 -0300

    Update main.m

--- DoggyAlert/DoggyAlert/main.m
+++ DoggyAlert/DoggyAlert/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/2/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 #import "AppDelegate.h"
