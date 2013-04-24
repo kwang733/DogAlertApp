c221871cd5c4be728b38987ac47a538d490b011c
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:01:31 -0300

    Update AppDelegate.h

--- DoggyAlert/DoggyAlert/AppDelegate.h
+++ DoggyAlert/DoggyAlert/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/2/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @class DogsListingViewController;
