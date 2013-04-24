0793194c7b546a5591274a940740e2d0892113ae
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:02:07 -0300

    Update CalenderViewController.m

--- DoggyAlert/DoggyAlert/CalenderViewController.m
+++ DoggyAlert/DoggyAlert/CalenderViewController.m
@@ -1,11 +1,3 @@
-//
-//  CalenderViewController.m
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "CalenderViewController.h"
 #import "Dogs.h"
 #import "Activities.h"
