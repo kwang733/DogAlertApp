f9518e448f98b1357a25257a275ba50c9474072f
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 22:01:56 -0300

    Update CalenderViewController.h

--- DoggyAlert/DoggyAlert/CalenderViewController.h
+++ DoggyAlert/DoggyAlert/CalenderViewController.h
@@ -1,11 +1,3 @@
-//
-//  CalenderViewController.h
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
 
