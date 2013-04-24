181d843a719cc923501eea55e86e54120045628d
kwang733 <kevinwang@fas.harvard.edu>
2013-04-23 21:59:28 -0300

    Update Activities.m

--- DoggyAlert/DoggyAlert/Activities.m
+++ DoggyAlert/DoggyAlert/Activities.m
@@ -1,11 +1,3 @@
-//
-//  Activities.m
-//  DoggyAlert
-//
-//  Created by Ayaz Alavi on 5/3/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "Activities.h"
 #import "ActivityTimeLog.h"
 #import "Dogs.h"
